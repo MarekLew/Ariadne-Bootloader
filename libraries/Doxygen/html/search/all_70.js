2a7a869177564da11918dbdc84581428baa58cfa
Stilianos Tsampas <zeronebones@gmail.com>
2012-12-03 04:49:18 +0200

    Somewhat updated documentation

--- libraries/Doxygen/html/search/all_70.js
+++ libraries/Doxygen/html/search/all_70.js
@@ -2,6 +2,9 @@ var searchData=
 [
   ['passsigisset',['passSigIsSet',['../class_net_e_e_p_r_o_m_class.html#a818eead1b4beec0cdce536675cb2e21c',1,'NetEEPROMClass']]],
   ['portsigisset',['portSigIsSet',['../class_net_e_e_p_r_o_m_class.html#a54edc45508ea361553540313925debfd',1,'NetEEPROMClass']]],
-  ['print',['print',['../class_net_e_e_p_r_o_m_class.html#ad299347b9ddaf920b9e534793ba8d2f7',1,'NetEEPROMClass']]],
-  ['printall',['printAll',['../class_net_e_e_p_r_o_m_class.html#abec5e6a6b08f9c0abd44f01797c7e039',1,'NetEEPROMClass']]]
+  ['print',['print',['../class_net_e_e_p_r_o_m_class.html#a5d8f1ef54c7b4c9af76a80ff8038e75d',1,'NetEEPROMClass']]],
+  ['printall',['printAll',['../class_net_e_e_p_r_o_m_class.html#ae1115f0ecdbc4b9025a8877365b3da3d',1,'NetEEPROMClass']]],
+  ['printnet',['printNet',['../class_net_e_e_p_r_o_m_class.html#a2629f40df636395ffd2ba81cde49e6cf',1,'NetEEPROMClass']]],
+  ['printpass',['printPass',['../class_net_e_e_p_r_o_m_class.html#a982a1219fbb779f6d26a9ced24837047',1,'NetEEPROMClass']]],
+  ['printport',['printPort',['../class_net_e_e_p_r_o_m_class.html#aa1db291a5382a2ff75c23969d3d3ef0b',1,'NetEEPROMClass']]]
 ];
--- libraries/Doxygen/html/search/classes_6e.js
+++ libraries/Doxygen/html/search/classes_6e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['neteepromclass',['NetEEPROMClass',['../class_net_e_e_p_r_o_m_class.html',1,'']]]
+  ['neteepromclass',['NetEEPROMClass',['../class_net_e_e_p_r_o_m_class.html',1,'']]],
+  ['neweepromclass',['NewEEPROMClass',['../class_new_e_e_p_r_o_m_class.html',1,'']]]
 ];
